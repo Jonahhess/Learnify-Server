4744996f78f0a45db4136972dc1a497970d4d804
yoavuzan <yoav547@gmail.com>
2025-09-19 10:53:54 +0300

    add questionsController with all the CRUD function

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,13 @@ const userRoutes = require('./users');
 const courseRoutes = require('./courses');
 const coursewareRoutes = require('./coursewares');
 const reviewCardRoutes = require('./reviewCards');
+const questionRoutes = require('./questions');
 
 router.use('/user', userRoutes);
 router.use('/courses', courseRoutes);
 router.use('/coursewares', coursewareRoutes);
 router.use('/review-cards', reviewCardRoutes);
+router.use('/questions', questionRoutes);
 
 // TODO: Add API routes here
 router.get('/', (req, res) => {
