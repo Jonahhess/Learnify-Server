14b520f4af36cc1624f7d7e6fed0690fb9d61c6f
jonahhess <jonahhessdev@gmail.com>
2025-09-22 00:25:12 +0300

    added coursewareId to reviewCard schema

--- models/reviewCards.js
+++ models/reviewCards.js
@@ -1,35 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const reviewCardSchema = new mongoose.Schema({
-  courseId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Course',
-    required: true
+const reviewCardSchema = new mongoose.Schema(
+  {
+    courseId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    coursewareId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Courseware",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    question: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Question",
+      required: true,
+    },
+    successes: {
+      type: Number,
+      default: 0,
+    },
+    reviews: {
+      type: Number,
+      default: 0,
+    },
+    nextReviewDate: {
+      type: Date,
+      required: true,
+    },
   },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  question: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Question',
-    required: true
-  },
-  successes: {
-    type: Number,
-    default: 0
-  },
-  reviews: {
-    type: Number,
-    default: 0
-  },
-  nextReviewDate: {
-    type: Date,
-    required: true
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
-module.exports = mongoose.model('reviewCards', reviewCardSchema);
+module.exports = mongoose.model("reviewCards", reviewCardSchema);
