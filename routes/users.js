1c31d7264515403baa1ab2a98706e1d8cfc2574e
jonahhess <jonahhessdev@gmail.com>
2025-09-22 09:56:29 +0300

    added 4 routes for user app interaction

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,47 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../middleware/auth');
-const validateNewUser = require('../middleware/validateNewUser');
-const { 
-  createUser, 
+const auth = require("../middleware/auth");
+const validateNewUser = require("../middleware/validateNewUser");
+const {
+  createUser,
   loginUser,
-  getUserById, 
-  updateUser, 
-  deleteUser 
-} = require('../controllers/userController');
-
+  getUserById,
+  updateUser,
+  deleteUser,
+  startCourse,
+  startCourseware,
+  submitCourseware,
+  batchSubmitReviewCards,
+} = require("../controllers/userController");
 
 // Create a new user
-router.post('/', validateNewUser, createUser);
+router.post("/", validateNewUser, createUser);
 
 // Login a user
-router.post('/login', loginUser);
+router.post("/login", loginUser);
+
+// interacting with the app
+
+// Add Course to user by Id
+router.post("/course/:id", auth, startCourse);
+
+// Submit Courseware by Id
+router.post("/coursewares/:id", auth, startCourseware);
+
+router.post("/coursewares/:id/submit", auth, submitCourseware);
+
+// Submit Review Cards
+router.post("/review", auth, batchSubmitReviewCards);
+
+// end of app section
 
 // Get a user by ID
-router.get('/:id', auth, getUserById);
+router.get("/:id", auth, getUserById);
 
 // Update a user by ID
-router.put('/:id', auth, updateUser);
+router.put("/:id", auth, updateUser);
 
 // Delete a user by ID
-router.delete('/:id', auth, deleteUser);
+router.delete("/:id", auth, deleteUser);
 
 module.exports = router;
