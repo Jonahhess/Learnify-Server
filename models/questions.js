c9e92267b95cc35ce54ca89090db24a5241ea5dc
jonahhess <jonahhessdev@gmail.com>
2025-09-25 00:58:01 +0300

    updated schema

--- models/questions.js
+++ models/questions.js
@@ -19,19 +19,17 @@ const questionSchema = new mongoose.Schema(
       type: [String],
       required: true,
     },
-    stats: {
-      likes: {
-        type: Number,
-        default: 0,
-      },
-      dislikes: {
-        type: Number,
-        default: 0,
-      },
-      views: {
-        type: Number,
-        default: 0,
-      },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    dislikes: {
+      type: Number,
+      default: 0,
+    },
+    views: {
+      type: Number,
+      default: 0,
     },
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
