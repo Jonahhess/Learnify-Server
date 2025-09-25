bd1e9b435b195c86a7fd8638c50f54aa46a7951b
jonahhess <jonahhessdev@gmail.com>
2025-09-25 12:10:42 +0300

    typo

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,4 @@
-
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
   // Get token from cookie
@@ -7,15 +6,15 @@ module.exports = function (req, res, next) {
 
   // Check if not token
   if (!token) {
-    return res.status(401).json({ message: 'No token, authorization denied' });
+    return res.status(401).json({ message: "No token, authorization denied" });
   }
 
   // Verify token
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user;
+    req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ message: 'Token is not valid' });
+    res.status(401).json({ message: "Token is not valid" });
   }
 };
