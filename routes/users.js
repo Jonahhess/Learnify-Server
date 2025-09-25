11cd55487fa7e88d66929500c2bfc4889bad991b
jonahhess <jonahhessdev@gmail.com>
2025-09-25 16:12:40 +0300

    added middleware - hope it works

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,7 @@ const {
   submitCourseware,
   batchSubmitReviewCards,
 } = require("../controllers/userController");
+const validateUser = require("../middleware/validateUser");
 
 // Create a new user
 router.post("/", validateNewUser, createUser);
@@ -26,23 +27,28 @@ router.post("/login", loginUser);
 router.post("/:id/courses", auth, startCourse);
 
 // Submit Courseware by id
-router.put("/:id/coursewares/:coursewareId", auth, submitCourseware);
+router.put(
+  "/:id/coursewares/:coursewareId",
+  auth,
+  validateUser,
+  submitCourseware
+);
 
 // Start Courseware by Id
-router.post("/:id/coursewares", auth, startCourseware);
+router.post("/:id/coursewares", auth, validateUser, startCourseware);
 
 // Submit Review Cards
-router.post("/:id/reviewcards", auth, batchSubmitReviewCards);
+router.post("/:id/reviewcards", auth, validateUser, batchSubmitReviewCards);
 
 // end of app section
 
 // Get a user by ID
-router.get("/:id", auth, getUserById);
+router.get("/:id", auth, validateUser, getUserById);
 
 // Update a user by ID
-router.put("/:id", auth, updateUser);
+router.put("/:id", auth, validateUser, updateUser);
 
 // Delete a user by ID
-router.delete("/:id", auth, deleteUser);
+router.delete("/:id", auth, validateUser, deleteUser);
 
 module.exports = router;
